6413987e8490d032b20d3622ea10c4c1fc5dcbf5
p1ch_jp <p1ch.jp@gmail.com>
2015-06-19 20:34:12 +0900

    トップページを英語仕様に変更

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,24 +16,30 @@ $(window).resize(function() {
   }, 250);
 });
 
-// $(window).scroll(function () {
-//   var top = $(this).scrollTop();
-//   parallaxBg(top);
-// });
-
 
 function fitHeaderSize () {
   var height = $(window).height();
-  $(".header").height(height);
-}
+  var width = $(window).width();
 
-// function parallaxBg (top) {
-//   $(".header").css({
-//     "background-position": '0px' + ' ' + - top / 15 + 'px'
-//   });
-//   $(".download").css({
-//     "background-position": '0px' + ' ' + - top / 15 + 'px'
-//   });
-// }
+  $(".header").height(height);
+  var movie_width = $("#header__movie").width();
+  var ne_margin = (movie_width - width) / 2;
+
+  var header_inner_width = $(".header__inner").width();
+  var header_inner_height = $(".header__inner").height();
+
+
+  $(".heaedr__filter").height(height);
+
+  // $("#header__movie").css({
+  //   "marginLeft": - ne_margin + 'px'
+  // });
+
+  $(".header__inner").css({
+    "marginTop": - header_inner_height / 2 + 'px',
+    "marginLeft": - header_inner_width / 2 + 'px'
+  });
+
+}
 
 });
